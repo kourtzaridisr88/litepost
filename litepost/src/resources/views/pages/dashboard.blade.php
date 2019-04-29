708d9c340aa440aebad13c8892a7c62d23a08f53
George Kourtzaridis <kourtzaridisr88@gmail.com>
2019-04-29 09:54:37 +0000

    dashboard

--- litepost/src/resources/views/pages/dashboard.blade.php
+++ litepost/src/resources/views/pages/dashboard.blade.php
@@ -3,102 +3,97 @@
 @section('title', 'Dashboard')
 
 @section('main')
-Main
-@endsection
-
-@section('main')
-<section class="section breadcrumb">
+<section class="section" style="background-color: #e9ecef;">
     <div class="container-fluid">
         <div class="row">
             <div class="col-12">
                 <ol class="breadcrumb">
-                    <li class="breadcrumb-item"><a href="#">Admin</a></li>
-                    <li class="breadcrumb-item active" aria-current="page">Dashboard</li>
+                    <li class="breadcrumb-item"><a href="{{ route('litepost.dashboard') }}">Admin</a></li>
                 </ol>
-            </div>
-        </div>
-    </div>
-</section>
+            </div><!-- ./col -->
+        </div><!-- ./row -->
+    </div><!-- ./container-fluid -->
+</section><!-- ./section -->
 
 <section class="google_data">
     <div class="container-fluid">
         <div class="row">
-        
-            <div class="col-md-4 col-sm-6 col-xs-12 cardcol">
+
+            <div class="col-md-6 col-sm-6 col-xs-12 cardcol mt-5">
                 <!-- TODAY -->
                 <div class="card stats">
                     <div class="card-body">
-                    <blockquote class="blockquote mb-0">
-                        <p class="value">{{$currentday['sessions'] == null ? "0" : $currentday['sessions']}}</p>
-                        <footer class="blockquote-footer">USERS</footer>
-                    </blockquote>
-                    <blockquote class="blockquote mb-0">
-                        <p class="value">{{$currentday['pageviews'] == null ? "0" : $currentday['pageviews']}}</p>
-                        <footer class="blockquote-footer">VIEWS</footer>
-                    </blockquote>
+                        <div class="py-2">
+                            <span class="value">{{$currentday['sessions'] == null ? "0" : $currentday['sessions']}}</span>
+                            <span class="blockquote-footer" style="display: inline;">USERS</span>
+                        </div>
+                        <div class="py-2">
+                            <span class="value">{{$currentday['pageviews'] == null ? "0" : $currentday['pageviews']}}</span>
+                            <span class="blockquote-footer" style="display: inline;">VIEWS</span>
+                        </div>
                     </div>
                     <div class="card-header dashboard">
                     TODAY
                     </div>
                 </div>
-            </div>    
+            </div>   
 
             <!-- WEEK -->
-            <div class="col-md-4 col-sm-6 col-xs-12 cardcol"> 
-                    <div class="card stats">
+            <div class="col-md-6 col-sm-6 col-xs-12 cardcol mt-5">
+                <div class="card stats">
                     <div class="card-body">
-                    <blockquote class="blockquote mb-0">
-                        <p class="value">{{$currentweek['sessions'] == null ? "0" : $currentweek['sessions'] }}</p>
-                        <footer class="blockquote-footer">USERS</footer>
-                    </blockquote>
-                    <blockquote class="blockquote mb-0">
-                        <p class="value">{{$currentweek['pageviews'] == null ? "0" : $currentweek['pageviews'] }}</p>
-                        <footer class="blockquote-footer">VIEWS</footer>
-                    </blockquote>
+                        <div class="py-2">
+                            <span class="value">{{$currentweek['sessions'] == null ? "0" : $currentweek['sessions'] }}</span>
+                            <span class="blockquote-footer" style="display: inline;">USERS</span>
+                        </div>
+                        <div class="py-2">
+                            <span class="value">{{$currentweek['pageviews'] == null ? "0" : $currentweek['pageviews'] }}</span>
+                            <span class="blockquote-footer" style="display: inline;">VIEWS</span>
+                        </div>
                     </div>
                     <div class="card-header dashboard">
                         LAST WEEK
                     </div>
                 </div>
-            </div>
+            </div> 
 
             <!-- MONTH -->
-            <div class="col-md-4 col-sm-6 col-xs-12 cardcol"> 
+            <div class="col-md-6 col-sm-6 col-xs-12 cardcol mt-5">
                 <div class="card stats">
                     <div class="card-body">
-                    <blockquote class="blockquote mb-0">
-                        <p class="value">{{$currentmonth['sessions'] == null ? "0" : $currentmonth['sessions']}}</p>
-                        <footer class="blockquote-footer">USERS</footer>
-                    </blockquote>
-                    <blockquote class="blockquote mb-0">
-                        <p class="value">{{$currentmonth['pageviews'] == null ? "0" : $currentmonth['pageviews']}}</p>
-                        <footer class="blockquote-footer">VIEWS</footer>
-                    </blockquote>
+                        <div class="py-2">
+                            <span class="value">{{$currentmonth['sessions'] == null ? "0" : $currentmonth['sessions']}}</span>
+                            <span class="blockquote-footer" style="display: inline;">USERS</span>
+                        </div>
+                        <div class="py-2">
+                            <span class="value">{{$currentmonth['pageviews'] == null ? "0" : $currentmonth['pageviews']}}</span>
+                            <span class="blockquote-footer" style="display: inline;">VIEWS</span>
+                        </div>
                     </div>
                     <div class="card-header dashboard">
                     LAST MONTH
                     </div>
                 </div>
-            </div>
+            </div> 
 
             <!-- YEAR -->
-            <div class="col-md-4 col-sm-6 col-xs-12 cardcol"> 
+            <div class="col-md-6 col-sm-6 col-xs-12 cardcol mt-5">
                 <div class="card stats">
                     <div class="card-body">
-                    <blockquote class="blockquote mb-0">
-                        <p class="value">{{$currentyear['sessions'] == null ? "0" : $currentyear['sessions']}}</p>
-                        <footer class="blockquote-footer">USERS</footer>
-                    </blockquote>
-                    <blockquote class="blockquote mb-0">
-                        <p class="value">{{$currentyear['pageviews'] == null ? "0" : $currentyear['pageviews']}}</p>
-                        <footer class="blockquote-footer">VIEWS</footer>
-                    </blockquote>
+                        <div class="py-2">
+                            <span class="value">{{$currentyear['sessions'] == null ? "0" : $currentyear['sessions']}}</span>
+                            <span class="blockquote-footer" style="display: inline;">USERS</span>
+                        </div>
+                        <div class="py-2">
+                            <span class="value">{{$currentyear['pageviews'] == null ? "0" : $currentyear['pageviews']}}</span>
+                            <span class="blockquote-footer" style="display: inline;">VIEWS</span>
+                        </div>
                     </div>
                     <div class="card-header dashboard">
-                    LAST YEAR
+                        YEAR
                     </div>
                 </div>
-            </div>
+            </div>     
         </div>
     </div>
 </section>
